2da30610f4ecca65f19bba08cbfc797f989e0a8a
Saksham Sharma <53181089+sakshams02@users.noreply.github.com>
2019-08-24 12:48:58 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+from keep_alive import keep_alive
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("I'm in")
+    print(client.user)
+
+@client.event
+async def on_message(message):
+    if message.author != client.user:
+        await client.send_message(message.channel, message.content[::-1])
+
+keep_alive()
+token = os.environ.get("SAM_DISCORD_SECRET")
+client.run(token)
